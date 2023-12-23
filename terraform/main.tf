83ac990a9d7f1f16691915d653c2db161f5153a0
Josh Owens <me@jowens.dev>
2023-12-22 23:22:51 -0600

    fixing metallb deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,31 +10,6 @@ provider "helm" {
 
 resource "helm_release" "metallb" {
   name       = "metallb"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://metallb.github.io/metallb"
   chart      = "metallb"
-
-  set {
-    name  = "controller.image.tag"
-    value = "v0.9.6"
-  }
-
-  set {
-    name  = "speaker.image.tag"
-    value = "v0.9.6"
-  }
-
-  set {
-    name  = "configInline.address-pools[0].name"
-    value = "default"
-  }
-
-  set {
-    name  = "configInline.address-pools[0].protocol"
-    value = "layer2"
-  }
-
-  set {
-    name  = "configInline.address-pools[0].addresses[0]"
-    value = var.address_range
-  }
 }

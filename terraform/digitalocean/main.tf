a47e88256a93427811051aee9f171b0d2bc90711
Josh Owens <me@jowens.dev>
2023-12-28 21:28:06 -0600

    firewall rules for droplet

--- terraform/digitalocean/main.tf
+++ terraform/digitalocean/main.tf
@@ -26,3 +26,52 @@ resource "digitalocean_droplet" "web" {
 output "ip_address" {
     value = digitalocean_droplet.web.ipv4_address
 }
+
+resource "digitalocean_firewall" "web_fw" {
+    name = "web-firewall"
+
+    droplet_ids = [digitalocean_droplet.web.id]
+
+    inbound_rule {
+        protocol           = "tcp"
+        port_range         = "443"
+        source_addresses   = ["0.0.0.0/0", "::/0"]
+    }
+    
+    inbound_rule {
+        protocol           = "tcp"
+        port_range         = "22"
+        source_addresses   = ["0.0.0.0/0", "::/0"]
+    }
+
+    # Required only for Flannel VXLAN
+    inbound_rule {
+        protocol           = "udp"
+        port_range         = "8472"
+        source_addresses   = var.source_addresses
+    }
+    
+    # Kubelete Metrics server
+    inbound_rule {
+        protocol           = "tcp"
+        port_range         = "10250"
+        source_addresses   = var.source_addresses
+    }
+
+    outbound_rule {
+        protocol           = "tcp"
+        port_range         = "1-65535"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    outbound_rule {
+        protocol           = "udp"
+        port_range         = "1-65535"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    outbound_rule {
+        protocol           = "icmp"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+}
--- terraform/digitalocean/variables.tf
+++ terraform/digitalocean/variables.tf
@@ -7,3 +7,8 @@ variable "ssh_key_name" {
     description = "Name of the SSH key to use in Digital Ocean"
     type = string 
 }
+
+variable "source_addresses" {
+  description = "Source IP addresses of k3 nodes for fire wall rules"
+  type        = list(string)
+}
